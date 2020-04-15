593f34722166540bb4b67ec8dfacf11b1b796873
Michiel <63006492+Michiel1973@users.noreply.github.com>
2020-04-15 22:07:32 +0200

    disable mobs until ready to deploy

--- mods/mobs_monster/zzzspawn.lua
+++ mods/mobs_monster/zzzspawn.lua
@@ -12,18 +12,18 @@
 -- Nether      -25000 to -30000
 
 --mobs:spawn_specific (name, nodes, neighbors, min_light, max_light, interval, chance, active_object_count, min_height, max_height, day_toggle, on_spawn)
-mobs:spawn_specific ("mobs_monster:zombie", "default:dirt_with_grass", "air", 0, 15, 10, 30, 10, -100, 100)
-mobs:spawn_specific ("mobs_monster:tree_monster", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
-mobs:spawn_specific ("mobs_monster:stone_monster", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
-mobs:spawn_specific ("mobs_monster:spider", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
-mobs:spawn_specific ("mobs_monster:stone_monster", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
-mobs:spawn_specific ("mobs_monster:slime_big", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
-mobs:spawn_specific ("mobs_monster:magma_cube_big", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
-mobs:spawn_specific ("mobs_monster:sand_monster", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
-mobs:spawn_specific ("mobs_monster:oerkki", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
-mobs:spawn_specific ("mobs_monster:lava_flan", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
+--mobs:spawn_specific ("mobs_monster:zombie", "default:dirt_with_grass", "air", 0, 15, 10, 30, 10, -100, 100)
+--mobs:spawn_specific ("mobs_monster:tree_monster", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
+--mobs:spawn_specific ("mobs_monster:stone_monster", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
+--mobs:spawn_specific ("mobs_monster:spider", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
+--mobs:spawn_specific ("mobs_monster:stone_monster", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
+--mobs:spawn_specific ("mobs_monster:slime_big", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
+--mobs:spawn_specific ("mobs_monster:magma_cube_big", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
+--mobs:spawn_specific ("mobs_monster:sand_monster", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
+--mobs:spawn_specific ("mobs_monster:oerkki", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
+--mobs:spawn_specific ("mobs_monster:lava_flan", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
 -- CR DM       -8000  to -9999
 mobs:spawn_specific ("mobs_monster:dungeon_master", "caverealms:hot_cobble", "air", 0, 15, 60, 9000, 2, -8100, -9950)
-mobs:spawn_specific ("mobs_monster:dirt_monster", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
+--mobs:spawn_specific ("mobs_monster:dirt_monster", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
 
 
