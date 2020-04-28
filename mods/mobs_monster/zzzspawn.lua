58737faa9304d9239dd951993c5f110fabcb9a8b
Michiel <63006492+Michiel1973@users.noreply.github.com>
2020-04-28 13:12:29 +0200

    update monsters
    - dungeon master can also spawn on stone
    - oerkki in the Nether

--- mods/mobs_monster/zzzspawn.lua
+++ mods/mobs_monster/zzzspawn.lua
@@ -11,11 +11,20 @@
 -- Primordial  -19073 to -22032
 -- Nether      -25000 to -30000
 
+--mobs:spawn_specific (name, nodes, neighbors, min_light, max_light, interval, chance, active_object_count, min_height, max_height, day_toggle, on_spawn)
+
+--caverealms monsters
 mobs:spawn_specific ("mobs_monster:lava_flan", {"default:lava_source", "default:lava_flowing"}, "air", 0, 13, 60, 100, 1, -8000, -6000)
 mobs:spawn_specific ("mobs_monster:magma_cube_big", {"default:lava_source", "default:lava_flowing"}, "air", 0, 13, 60, 300, 1, -8000, -6000)
-mobs:spawn_specific ("mobs_monster:dungeon_master", "caverealms:hot_cobble", "air", 0, 13, 60, 9000, 1, -8100, -9950)
 
---mobs:spawn_specific (name, nodes, neighbors, min_light, max_light, interval, chance, active_object_count, min_height, max_height, day_toggle, on_spawn)
+-- caverealms dungeon master layer
+mobs:spawn_specific ("mobs_monster:dungeon_master", {"caverealms:hot_cobble", "default:stone"}, "air", 0, 13, 60, 9000, 1, -8100, -9950)
+
+-- Nether
+mobs:spawn_specific ("mobs_monster:oerkki", "nether:rack", "air", 0, 14, 60, 1000, 2, -25000, 31000)
+
+
+
 --mobs:spawn_specific ("mobs_monster:zombie", "default:dirt_with_grass", "air", 0, 15, 10, 30, 10, -100, 100)
 --mobs:spawn_specific ("mobs_monster:tree_monster", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
 --mobs:spawn_specific ("mobs_monster:stone_monster", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
@@ -24,7 +33,7 @@ mobs:spawn_specific ("mobs_monster:dungeon_master", "caverealms:hot_cobble", "ai
 --mobs:spawn_specific ("mobs_monster:slime_big", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
 
 --mobs:spawn_specific ("mobs_monster:sand_monster", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
---mobs:spawn_specific ("mobs_monster:oerkki", "default:dirt_with_grass", "air", 0, 10, 30, 10, 10, -100, 100)
+
 
 -- CR DM       -8000  to -9999
 mobs:spawn_specific ("mobs_monster:dungeon_master", "caverealms:hot_cobble", "air", 0, 15, 60, 9000, 2, -8100, -9950)
