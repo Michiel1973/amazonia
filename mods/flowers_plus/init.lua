8deec6b07eb48a1cca3d506a3fd2d1ec768aa301
Michiel <63006492+Michiel1973@users.noreply.github.com>
2020-04-30 12:49:01 +0200

    reduce sunflowers and lilies spawn

--- mods/flowers_plus/init.lua
+++ mods/flowers_plus/init.lua
@@ -9,12 +9,12 @@ flowers_plus = {}
 local SPAWN_DELAY = 2000
 local SPAWN_CHANCE = 400
 local flowers_seed_diff = 329
-local lilies_max_count = 100
-local lilies_rarity = 33
-local seaweed_max_count = 100
-local seaweed_rarity = 33
+local lilies_max_count = 50
+local lilies_rarity = 50
+local seaweed_max_count = 50
+local seaweed_rarity = 50
 local sunflowers_max_count = 5
-local sunflowers_rarity = 25
+local sunflowers_rarity = 50
 
 -- register the various rotations of waterlilies
 
