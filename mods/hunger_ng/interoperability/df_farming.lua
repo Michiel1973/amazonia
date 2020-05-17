44d60a9172766a2301300abed09c279f1352d761
Michiel <63006492+Michiel1973@users.noreply.github.com>
2020-05-17 20:03:18 +0200

    hunger_ng: add df_farming items

---
+++ mods/hunger_ng/interoperability/df_farming.lua
@@ -0,0 +1,53 @@
+local add = hunger_ng.add_hunger_data
+
+add('df_farming:cave_bread', { satiates = 1.0 })
+add('df_farming:plump_helmet_1', { satiates = 1.0 })
+add('df_farming:plump_helmet_2', { satiates = 1.5 })
+add('df_farming:plump_helmet_3', { satiates = 2.0 })
+add('df_farming:plump_helmet_4', { satiates = 2.5 })
+add('df_farming:plump_helmet_4_picked', { satiates = 2.5 })
+
+
+add('df_farming:cave_flour_simple_meal', { satiates = 3.0, heals = 1.0 })
+add('df_farming:cave_flour_medium_meal', { satiates = 6.0, heals = 2.0 })
+add('df_farming:cave_flour_complex_meal', { satiates = 9.0, heals = 4.0 })
+
+add('df_farming:cave_wheat_seed_simple_meal', { satiates = 3.0, heals = 1.0 })
+add('df_farming:cave_wheat_seed_medium_meal', { satiates = 6.0, heals = 2.0 })
+add('df_farming:cave_wheat_seed_complex_meal', { satiates = 9.0, heals = 4.0 })
+
+add('df_farming:sweet_pod_seed_simple_meal', { satiates = 3.0, heals = 1.0 })
+add('df_farming:sweet_pod_seed_medium_meal', { satiates = 6.0, heals = 2.0 })
+add('df_farming:sweet_pod_seed_complex_meal', { satiates = 9.0, heals = 4.0 })
+
+add('df_farming:sugar_simple_meal', { satiates = 3.0, heals = 1.0 })
+add('df_farming:sugar_medium_meal', { satiates = 6.0, heals = 2.0 })
+add('df_farming:sugar_complex_meal', { satiates = 9.0, heals = 4.0 })
+
+add('df_farming:plump_helmet_simple_meal', { satiates = 3.0, heals = 1.0 })
+add('df_farming:plump_helmet_medium_meal', { satiates = 6.0, heals = 2.0 })
+add('df_farming:plump_helmet_complex_meal', { satiates = 9.0, heals = 4.0 })
+
+add('df_farming:plump_helmet_spawn_simple_meal', { satiates = 3.0, heals = 1.0 })
+add('df_farming:plump_helmet_spawn_medium_meal', { satiates = 6.0, heals = 2.0 })
+add('df_farming:plump_helmet_spawn_complex_meal', { satiates = 9.0, heals = 4.0 })
+
+add('df_farming:quarry_bush_leaves_simple_meal', { satiates = 3.0, heals = 1.0 })
+add('df_farming:quarry_bush_leaves_medium_meal', { satiates = 6.0, heals = 2.0 })
+add('df_farming:quarry_bush_leaves_complex_meal', { satiates = 9.0, heals = 4.0 })
+
+add('df_farming:quarry_bush_seed_simple_meal', { satiates = 3.0, heals = 1.0 })
+add('df_farming:quarry_bush_seed_medium_meal', { satiates = 6.0, heals = 2.0 })
+add('df_farming:quarry_bush_seed_complex_meal', { satiates = 9.0, heals = 4.0 })
+
+add('df_farming:dimple_cup_seed_simple_meal', { satiates = 3.0, heals = 1.0 })
+add('df_farming:dimple_cup_seed_medium_meal', { satiates = 6.0, heals = 2.0 })
+add('df_farming:dimple_cup_seed_complex_meal', { satiates = 9.0, heals = 4.0 })
+
+add('df_farming:pig_tail_seed_simple_meal', { satiates = 3.0, heals = 1.0 })
+add('df_farming:pig_tail_seed_medium_meal', { satiates = 6.0, heals = 2.0 })
+add('df_farming:pig_tail_seed_complex_meal', { satiates = 9.0, heals = 4.0 })
+
+add('df_farming:dwarven_syrup_bucket_simple_meal', { satiates = 3.0, heals = 1.0 })
+add('df_farming:dwarven_syrup_bucket_medium_meal', { satiates = 6.0, heals = 2.0 })
+add('df_farming:dwarven_syrup_bucket_complex_meal', { satiates = 9.0, heals = 4.0 })
