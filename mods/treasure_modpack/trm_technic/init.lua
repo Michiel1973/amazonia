0450e74232d31273675e21d83ad6562d16bfae66
Michiel <63006492+Michiel1973@users.noreply.github.com>
2020-05-17 22:35:58 +0200

    random treasures in newly generated areas
    add treasurer https://forum.minetest.net/viewtopic.php?t=7292
    and a bunch of custom treasure mods

---
+++ mods/treasure_modpack/trm_technic/init.lua
@@ -0,0 +1,19 @@
+-- registers technic tools
+
+treasurer.register_treasure("technic:water_can",0.01,3,nil,nil,"minetool")
+treasurer.register_treasure("technic:lava_can",0.01,5,nil,nil,"minetool")
+treasurer.register_treasure("technic:chainsaw",0.01,7,nil,nil,"minetool")
+treasurer.register_treasure("technic:mining_drill",0.01,5,nil,nil,"minetool")
+treasurer.register_treasure("technic:mining_drill_mk2",0.001,8,nil,nil,"minetool")
+treasurer.register_treasure("technic:mining_drill_mk3",0.0001,10,nil,nil,"minetool")
+treasurer.register_treasure("technic:laser_mk1",0.01,5,nil,nil,"minetool")
+treasurer.register_treasure("technic:laser_mk2",0.001,8,nil,nil,"minetool")
+treasurer.register_treasure("technic:laser_mk3",0.0001,10,nil,nil,"minetool")
+treasurer.register_treasure("technic:prospector",0.01,5,nil,nil,"minetool")
+treasurer.register_treasure("technic:treetap",0.01,3,nil,nil,"minetool")
+treasurer.register_treasure("technic:vacuum",0.01,6,nil,nil,"minetool")
+
+
+
+
+
