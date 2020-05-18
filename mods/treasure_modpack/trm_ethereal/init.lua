3bda227e7d1585927eb7bc05f5c5086f90d50863
Michiel <63006492+Michiel1973@users.noreply.github.com>
2020-05-18 14:04:04 +0200

    treasure mod adjustments
    add tool category to chests
    reduce spawn chances in open world (again)
    move technic items to tool category
    add additional ethereal items

--- mods/treasure_modpack/trm_ethereal/init.lua
+++ mods/treasure_modpack/trm_ethereal/init.lua
@@ -8,6 +8,29 @@ treasurer.register_treasure("ethereal:pine_nuts",0.05,3,{1,5},nil,"food")
 treasurer.register_treasure("ethereal:coconut",0.05,1,{1,2},nil,"food")
 treasurer.register_treasure("ethereal:golden_apple",0.01,8,{1,5},nil,"food")
 treasurer.register_treasure("ethereal:hearty_stew",0.02,5,{1,3},nil,"food")
+treasurer.register_treasure("ethereal:mushroom_soup",0.02,5,{1,3},nil,"food")
+
+treasurer.register_treasure("ethereal:stone_ladder",0.02,5,{5,25},nil,"building_block")
+treasurer.register_treasure("ethereal:glostone",0.01,5,{1,5},nil,"building_block")
+treasurer.register_treasure("ethereal:bamboo_block",0.02,5,{1,5},nil,"building_block")
+
+treasurer.register_treasure("ethereal:candle",0.02,5,{1,3},nil,"deco")
+treasurer.register_treasure("ethereal:paper_wall",0.02,5,{1,5},nil,"deco")
+
+treasurer.register_treasure("ethereal:bamboo_sprout",0.02,5,nil,nil,"seed")
+treasurer.register_treasure("ethereal:sakura_sapling",0.02,5,nil,nil,"seed")
+treasurer.register_treasure("ethereal:orange_tree_sapling",0.02,5,nil,nil,"seed")
+treasurer.register_treasure("ethereal:redwood_sapling",0.02,5,nil,nil,"seed")
+treasurer.register_treasure("ethereal:banana_tree_sapling",0.02,5,nil,nil,"seed")
+treasurer.register_treasure("ethereal:yellow_tree_sapling",0.01,8,nil,nil,"seed")
+
+treasurer.register_treasure("ethereal:crystal_spike",0.003,9,{1,3},nil,"crafting_component")
+treasurer.register_treasure("ethereal:crystal_ingot",0.006,10,{1,3},nil,"crafting_component")
+
+treasurer.register_treasure("ethereal:light_staff",0.02,9,nil,nil,"tool")
+treasurer.register_treasure("ethereal:shovel_crystal",0.01,8,nil,nil,"tool")
+treasurer.register_treasure("ethereal:crystal_gilly_staff",0.02,5,nil,nil,"tool")
+
 
 
 
